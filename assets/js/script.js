749b00105bcefaf158f2492c789db538d3d9f43e
Renz Justine Gallego <renzjustine.gallego@gmail.com>
2018-09-18 21:14:07 +0800

    portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$(window).scroll(function(){
+		if($(window).scrollTop()>300){
+			$('nav').addClass('red');
+		}else{
+			$('nav').removeClass('red');
+		}
+
+	});
+
+});
